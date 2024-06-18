462a056c0b16c52b13fdaa85327b5b5c25e861f4
Parveen <parveenkanwal53@googlemail.comgit config --global user.name Parveengit config --global user.email parveenkanwal53@googlemail.com>
2024-06-18 16:17:49 +0100

    Added hello Leon

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Parveen"));
+         System.out.println(sayHelloToSomeone("Leon"));
         System.out.println(sayGoodbye());
     }
 
